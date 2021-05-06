c3cac0a77c06fec8351321de8d2a23038120af98
jjm2317 <wlaks2317@gmail.com>
2021-05-07 01:30:44 +0900

    feat: Solve bubbleSort, binary

---
+++ start.js
@@ -0,0 +1,13 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", (line) => {
+  input.push(line);
+  rl.close();
+}).on("close", () => {});
